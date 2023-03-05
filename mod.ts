978a2538efbc1d4f3600375ada2a8d8290bf61e9
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 14:11:05 +0100

    move all source files into ./src for better organisation + export all things from the ./src files in ./mod.ts, bc. this is best practice on deno.land/x

---
+++ mod.ts
@@ -0,0 +1,7 @@
+/**
+ * The main entrypoint for this deno lib, as best practice on deno.land/x
+ */
+
+export * from "./src/client.ts"
+export * from "./src/logger.ts"
+export * from "./src/shared.ts"
