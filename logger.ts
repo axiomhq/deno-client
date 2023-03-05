c5a434e062ba363a6ae6c7b3362021490ba41f65
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 13:55:04 +0100

    logger.ts: access deno envs directly to make sure code accesses only env vars it needs + Check availability of AXIOM_DATASET env inisde sendLogs, to not throw the error immediately in initialization, even if it might not be needed
    => Idea for future: Maybe make the logger initialization explicit via a function
    to allow users of this lib to not initialize
    this axiom logger when developing locally, for example.

--- logger.ts
+++ logger.ts
@@ -3,7 +3,7 @@
 import { getIngestURL, throttle } from "./shared.ts";
 import { Client } from "./client.ts";
 
-const env = Deno.env.toObject();
+const AXIOM_DATASET = Deno.env.get('AXIOM_DATASET');
 const url = getIngestURL();
 const throttledSendLogs = throttle(sendLogs, 1000);
 
@@ -34,16 +34,21 @@ export const log = {
   flush: sendLogs,
 };
 
+
 async function sendLogs() {
   if (!logEvents.length) {
     return;
+  }
+
+  if (!AXIOM_DATASET) {
+    throw new Error(`You can't send/flush logs, bc. env var AXIOM_DATASET is not available!`);
   }
 
   try {
     const client = new Client({ url });
     await client.ingest({
       events: logEvents,
-      dataset: env.AXIOM_DATASET,
+      dataset: AXIOM_DATASET,
     });
   } catch (e) {
     console.error(e);
